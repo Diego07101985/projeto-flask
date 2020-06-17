9325ef60803577116a85aec24cbc207dc3e017b2
dsalles <dsalles@corp.globo.com>
2020-06-17 00:46:01 -0300

    Teste repositorio de usuario

--- desafio/src/repositorys.py
+++ desafio/src/repositorys.py
@@ -1,23 +1,50 @@
 from src.models import User
 from desafio import session_scope
 
+from src.models import User
+from desafio import session_scope
+from sqlalchemy import inspect
+
 
 class RepositoryUsers:
-    def __init__(self, session):
-        self.session = session
+    def __init__(self):
         pass
 
     def get_all_user(self):
         with session_scope() as session:
             return session.query(User).all()
 
+    def get_user(self, user):
+        with session_scope() as session:
+            user = session.query(User).filter(
+                User.username == user.username).first()
+        return user
+
     def insert(self, user):
         with session_scope() as session:
             session.add(user)
+            user = session.query(User).filter(
+                User.username == user.username).first()
+            return user.id
 
     def update(self, user):
         with session_scope() as session:
+            print(user)
+            print(f' Nome usuario: {user.username}')
             session.query(User).filter(
-                User.username == user.username).update({"email": user.email})
+                User.username == user.username). \
+                update({"email": user.email})
 
-        return user
+            update_user = session.query(User).filter(
+                User.username == user.username).first()
+        return update_user
+
+    def delete(self, user):
+        with session_scope() as session:
+            user = session.query(User).filter(
+                User.username == user.username).first()
+            session.delete(user)
+            update_user = session.query(User).filter(
+                User.username == user.username).first()
+
+        return update_user
